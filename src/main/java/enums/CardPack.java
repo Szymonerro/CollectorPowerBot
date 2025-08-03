126097bc0032ff985b2edd137af2f2a4c9289d0a
Szymonerro <artox_27@o2.pl>
2025-08-03 08:05:01 +0200

    update

--- src/main/java/enums/CardPack.java
+++ src/main/java/enums/CardPack.java
@@ -1,10 +1,10 @@
 package enums;
 
 public enum CardPack {
-    COLLECTION_AMAZON_S1("KqrL7Q4P"),
-    DRAGON_S1("GVx9zg5z"),
-    MONSTERS_S1("X2rmzxV6"),
-    EGYPT_OF_DESIRE("LWrwzQb0");
+    COLLECTION_AMAZON_S1("KqrL7Q4P/"),
+    DRAGON_S1("GVx9zg5z/"),
+    MONSTERS_S1("X2rmzxV6/"),
+    EGYPT_OF_DESIRE("LWrwzQb0/");
 
     private final String path;
 
